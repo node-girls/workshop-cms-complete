844053b57fd32db2c657f481fc24629fbe492bf4
Mina <wilhelmina.jane@gmail.com>
2016-02-19 20:02:02 +0000

    adds step4 and public directory

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 var http = require('http');
+var fs = require('fs');
 
-
-var message = 'I am so happy to be part of Node-Girls workshop!';
+var message;
 
 function handler (request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(message);
-    response.end();
+    var endpoint = request.url;
+    console.log(endpoint);
+
+    if (endpoint === '/') {
+
+        // serve home page
+        fs.readFile(__dirname + '/public/index.html', 'utf-8', function (error, file) {
+
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write(file);
+            response.end();
+        });
+    } else if (endpoint === '/node') {
+
+        message = "I love node!";
+
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(message);
+        response.end();
+    } else if (endpoint === '/girls') {
+
+        message = "Node Girls is cool!";
+
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(message);
+        response.end();
+    }
 }
 
 var server = http.createServer(handler);
