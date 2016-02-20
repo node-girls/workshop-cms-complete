862299e9be62827fa42cb5a28d9b80c515321199
Anita <a.nita@hotmail.co.uk>
2016-02-20 15:38:19 +0000

    step7

--- server.js
+++ server.js
@@ -1,67 +1,6 @@
 var http = require('http');
-var fs = require('fs');
-var querystring = require('querystring');
 
-var message;
-
-function handler (request, response) {
-
-    var endpoint = request.url;
-    console.log(endpoint);
-
-    if (endpoint === '/') {
-
-        // serve home page
-        fs.readFile(__dirname + '/public/index.html', function (error, file) {
-
-            response.writeHead(200, {"Content-Type": "text/html"});
-            response.write(file);
-            response.end();
-        });
-    } else if (endpoint === '/node') {
-
-        message = "I love node!";
-
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write(message);
-        response.end();
-
-    } else if (endpoint === '/girls') {
-
-        message = "Node Girls is cool!";
-
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write(message);
-        response.end();
-
-    } else if (endpoint === '/create-post') {
-
-        message = "";
-        
-        request.on("data", function(data) {
-            message += data;
-        });
-
-        request.on("end", function () {
-            message = querystring.parse(message);
-            console.log(message.blogpost);
-            response.end();
-        });
-
-    } else {
-
-        var pathToFile = __dirname + '/public' + endpoint;
-        var fileExtensionArray = endpoint.split('.');
-        var fileExtension = fileExtensionArray[1];
-
-        fs.readFile(pathToFile, function (error, file) {
-
-            response.writeHead(200, { "Content-Type": "text/" + fileExtension });
-            response.write(file);
-            response.end();
-        });
-    }
-}
+var handler = require('./src/handler.js');
 
 var server = http.createServer(handler);
 
