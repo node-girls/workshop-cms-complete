f75200bd0fafbc057e4a00d6de93f1e19c89ebe1
Anita <a.nita@hotmail.co.uk>
2016-02-21 15:15:43 +0000

    working version

--- src/handler.js
+++ src/handler.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var querystring = require('querystring');
 
 function handler (request, response) {
-    
+
     var endpoint = request.url;
 
     if (endpoint === '/') {
@@ -10,6 +10,10 @@ function handler (request, response) {
         var pathToIndex = __dirname + '/../public/index.html';
 
         fs.readFile(pathToIndex, function (error, file) {
+
+            if (error) {
+                throw error;
+            }
             response.writeHead(200, {"Content-Type": "text/html"});
             response.write(file);
             response.end();
@@ -17,44 +21,61 @@ function handler (request, response) {
     } else if (request.method === 'POST') {
 
         var data = "";
-        
-        request.on("data", function(chunk) {
-            data += chunk;
-            var post = querystring.parse(data);
-            post.timestamp = Date.now();
-            console.log(post);
 
-            fs.writeFile(__dirname + '/posts.json', JSON.stringify(post), function (err) {
-                if (err) {
-                    console.log(err);
-                }
-            })
+        request.on("data", function(chunk) {
+
+            data += chunk;
         });
 
         request.on("end", function () {
-        
-            var convertedData = querystring.parse(data);
-            fs.readFile(__dirname + "/posts.json", "utf8", function (err, data) {
 
-                if (err) {
-                    console.log(err);
-                } 
+            var newPost = querystring.parse(data);
 
-                response.writeHead(200);
-                response.write(data);
-                response.end();
-                
+            fs.readFile(__dirname + "/posts.json", "utf8", function (error, fileData) {
+
+                if (error) {
+                    console.log(error);
+                }
+                var blogposts = JSON.parse(fileData);
+                var currentTime = Date.now();
+
+                blogposts[currentTime] = newPost.blogpost;
+
+                fs.writeFile(__dirname + '/posts.json', JSON.stringify(blogposts), function (error) {
+
+                    if (error) {
+                        console.log(error);
+                    }
+                    response.writeHead(302, {"Location" : "/"});
+                    response.end();
+                });
             });
-            
         });
 
+    } else if (endpoint === '/get-posts'){
+
+        var pathToJSON = __dirname + '/posts.json';
+
+        fs.readFile(pathToJSON, function (error, blogposts) {
+
+            if (error) {
+                throw error;
+            }
+            response.writeHead(200, {"Content-Type": "text/json"});
+            response.write(blogposts);
+            response.end();
+        });
     } else {
+
         var pathToFile = __dirname + '/../public' + endpoint;
         var fileExtensionArray = endpoint.split('.');
         var fileExtension = fileExtensionArray[1];
 
         fs.readFile(pathToFile, function (error, file) {
 
+            if (error) {
+                throw error;
+            }
             response.writeHead(200, { "Content-Type": "text/" + fileExtension });
             response.write(file);
             response.end();
