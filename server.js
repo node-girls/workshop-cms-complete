1df73e9a13dd9a718c3f7ad0db7b87031586c78b
Anita <a.nita@hotmail.co.uk>
2016-02-21 12:00:46 +0000

    added handler to server step3

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var http = require('http');
 
-var server = http.createServer();
+var message = 'I am so happy to be part of Node-Girls workshop!';
+
+function handler (request, response) {
+
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(message); 
+  response.end(); 
+}
+
+var server = http.createServer(handler);
 
 server.listen(3000, function () {
 
